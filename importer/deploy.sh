595e9afdf362a9af2bba87ba423d85c00f221396
Johannes Schüth <j.schueth@gentics.com>
2020-02-12 18:15:09 +0100

    Update deployment scripts

--- importer/deploy.sh
+++ importer/deploy.sh
@@ -8,9 +8,9 @@ REF=${SHA1:0:4}
 PROJECT_DIR="`dirname \"$0\"`"
 cd $PROJECT_DIR
 
-PUSH_IMAGE=gtx-docker-apps.docker.apa-it.at/gentics/apps/musetech-app:$REF
-PULL_IMAGE=docker.apa-it.at/gentics/apps/musetech-app:$REF
-DEPLOYMENT=mesh-website-musetech-app
+PUSH_IMAGE=gentics/mesh-musetech-demo-importer
+PULL_IMAGE=$PUSH_IMAGE
+CRONJOB=mesh-website-musetech-cronjob
 
 echo -e "\nBuilding docker image"
 docker build -t $PUSH_IMAGE .
@@ -21,5 +21,4 @@ docker push $PUSH_IMAGE
 
 echo -e "\nReady to update on K8S. Press any key to continue"
 read
-kubectl -n core set image deployment/$DEPLOYMENT app=$PULL_IMAGE
-kubectl -n core rollout status deployment/$DEPLOYMENT
+kubectl -n core set image cronjob/$CRONJOB importer=$PULL_IMAGE
